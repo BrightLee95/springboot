c03c53ca491295c2be96e718cd5bcdbbfec0cb47
李洪亮 <478561533@qq.com>
2019-03-19 09:13:34 +0800

    Initial commit

---
+++ src/main/java/com/liang/springboot/web/HelloController.java
@@ -0,0 +1,14 @@
+package com.liang.springboot.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping(value = "/hello", method = RequestMethod.POST)
+	public String hello(){
+		return "Hello World!ssss";
+	}
+}
